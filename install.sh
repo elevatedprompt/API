a66f368cb492729c57f10680566f35603d8b0133
ichintu <ichintu@gmail.com>
2016-01-10 18:04:53 -0800

    Added support for PM2

--- install.sh
+++ install.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 # Install AdminPanel API
-npm install
-chmod +x runServer.sh
+sudo npm install pm2 -g
+sudo pm2 completion install
+sudo pm2 startup
+sudo chkconfig /etc/init.d/pm2-init.sh on
+sudo /etc/init.d/pm2-init.sh start
+sudo npm install
+sudo pm2 start index.js --name epstack-API
