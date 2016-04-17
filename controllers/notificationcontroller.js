f3e073d2a95a9d218cd43e216c2b7deefaddd1c5
cgoss <colin.goss@gmail.com>
2016-04-16 23:11:10 -0700

    update

--- controllers/notificationcontroller.js
+++ controllers/notificationcontroller.js
@@ -120,15 +120,15 @@ module.exports.UpdateNotification = function(req,res,next)
 
 function RegisterNotification(notification){
   var methodCall = notificationService + 'RegisterNotification';
-  var http = require('node-rest-client');
+  var Http = require('node-rest-client');
 
   var config = {headers:{
     "Content-type": "application/x-www-form-urlencoded; charset=utf-8"
     }};
 
   var data = "notificationName="+ encodeURIComponent(notification.notificationName);
-  var http = new Client();
-  
+  var http = new Http();
+
   http.post(methodCall,notification,config)
     .success(function(data)
       {
@@ -138,12 +138,12 @@ function RegisterNotification(notification){
 
 function UnregisterNotification(notificationName){
   var methodCall = notificationService + 'UnRegisterNotification';
-  var http = require('node-rest-client');
+  var Http = require('node-rest-client');
   var config = {headers:{
     "Content-type": "application/x-www-form-urlencoded; charset=utf-8"
     }};
     var data = "notificationName="+ encodeURIComponent(notificationName);
-    var http = new Client();
+    var http = new Http();
 
     http.post(methodCall,data,config)
     .success(function(data)
