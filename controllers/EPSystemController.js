bd791ab0342fade3b3408c2e84c0e74475d82505
cgoss <colin.goss@gmail.com>
2016-06-10 22:01:49 -0700

    adjusted script for scripts folder

--- controllers/EPSystemController.js
+++ controllers/EPSystemController.js
@@ -73,7 +73,7 @@ logEvent('Get Logstash File List');
        logEvent(file);
        logEvent(stat);
        logEvent(stat.isDirectory());
-       if (stat && file.isDirectory()) {
+       if (stat && stat.isDirectory()) {
            results = results.concat(file)
        } else results.push(file);
    });
