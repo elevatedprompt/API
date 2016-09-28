de8c197dd622737300683bfc29d51dd0667bc537
cgoss <colin.goss@gmail.com>
2016-09-28 13:27:02 -0700

    Updated to install from develop branch.
    Used only for developer install

--- installUI.sh
+++ installUI.sh
@@ -2,6 +2,6 @@
 # Install AdminPanel web
 sudo mkdir -p /var/www/epstack/
 cd /var/www/epstack/
-sudo git clone https://github.com/elevatedprompt/public_html
+sudo git clone -b develop https://github.com/elevatedprompt/public_html
 sudo chown -R apache:apache /var/www/epstack/public_html
 sudo chmod 755 /var/www
