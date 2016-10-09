c792369d2d89108614451953e3b157a4141e2fab
cgoss <colin.goss@gmail.com>
2016-10-09 09:16:49 -0700

    Added read of template and filter file directory.

--- index.js
+++ index.js
@@ -73,6 +73,8 @@ global.elastichost =  configuration.elastichost;
 global.notificationDirectory = configuration.notificationDirectory;
 global.elasticsearchLocation = configuration.elasticsearchLocation;
 global.logstashConfig = configuration.logstashConfig;
+global.logstashTemplates = configuration.logstashTemplates;
+global.logstashFilters = configuration.logstashFilters;
 global.cronJobDirectory = configuration.cronJobDirectory;
 logEvent(global.notificationDirectory);
 //TimeZone
